6b69c7399dee624a08ce98eed27a6769ef4acc49
Tatiana Petrova <petrova.tatiana.pavlovna@gmail.com>
2019-11-14 10:34:09 +0200

    16_cahee_nav

---
+++ 16_cahee_nav/src/js/bundle.js
@@ -0,0 +1,16 @@
+// $('.navigation__btn').on('click', function(e) {
+//   e.preventDefault();
+//   $(this).toggleClass(''navigation__btn'_active');
+//   $('.navigation__list').toggleClass(''navigation__list'_active');
+// });
+
+// document.querySelector('.navigation__btn').addEventListener('click', function (e) {
+//   e.preventDefault;
+//   this.classList.toggle('active');
+//   document.querySelector('.navigation__list').classList.toggle('db');
+// });
+
+// $('.navigation__btn').on('click', function(e) {
+//   e.preventDefault;
+//   $(this).toggleClass(''navigation__btn'_active');
+// });
