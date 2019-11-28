3489803b1706cf98de6c45cb886af4a3f3334d08
Tatiana Petrova <petrova.tatiana.pavlovna@gmail.com>
2019-11-28 18:18:24 +0200

    cahee full

--- js_tren/src/js/app.js
+++ js_tren/src/js/app.js
@@ -1,3 +1,12 @@
 alert('Добро пожаловать, '+prompt('Как Вас зовут?',''));
 
 prompt('Введите год вашего рождения','');
+
+
+let birthYear = parseInt(prompt('Введите год вашего рождения', ''));
+const currentYear = 2019;
+if (isFinite(birthYear)) {
+	alert(`Ваш возраст: ${currentYear - birthYear}`);
+} else {
+alert('Введите число');
+}
